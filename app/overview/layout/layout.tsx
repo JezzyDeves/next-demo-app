e56cd8efdd3793a04fc3c6a39a30a340b365a3a0
Jonathon Rivers <jdr2045@gmail.com>
2023-10-03 11:39:56 -0400

    Updated layout

--- app/overview/layout/layout.tsx
+++ app/overview/layout/layout.tsx
@@ -1,9 +1,22 @@
-import { PropsWithChildren } from "react";
+"use client";
+
+import { PropsWithChildren, useState } from "react";
 
 export default function Layout({ children }: PropsWithChildren) {
+  const [count, setCount] = useState(0);
   return (
-    <div className="flex min-h-screen items-center justify-center bg-slate-900 text-white text-center">
-      {children}
-    </div>
+    <>
+      <div className="flex flex-col min-h-screen items-center justify-center bg-slate-900 text-white text-center">
+        {children}
+        <div>
+          <button
+            className="rounded p-2 mt-2 bg-green-500 hover:bg-green-700 transition-colors text-white"
+            onClick={() => setCount(count + 1)}
+          >
+            Count is {count}
+          </button>
+        </div>
+      </div>
+    </>
   );
 }
